b556ca4c98d007e994623abedca58c769943e687
Paulus Schoutsen <balloob@gmail.com>
2020-01-03 23:59:15 +0100

    Mobile App add device tracker to person registering app

--- homeassistant/components/mobile_app/config_flow.py
+++ homeassistant/components/mobile_app/config_flow.py
@@ -1,7 +1,9 @@
 """Config flow for Mobile App."""
 from homeassistant import config_entries
+from homeassistant.components import person
+from homeassistant.helpers import entity_registry
 
-from .const import ATTR_DEVICE_NAME, DOMAIN
+from .const import ATTR_DEVICE_ID, ATTR_DEVICE_NAME, CONF_USER_ID, DOMAIN
 
 
 @config_entries.HANDLERS.register(DOMAIN)
@@ -23,6 +25,18 @@ class MobileAppFlowHandler(config_entries.ConfigFlow):
 
     async def async_step_registration(self, user_input=None):
         """Handle a flow initialized during registration."""
+        # Register device tracker entity and add to person registering app
+        ent_reg = await entity_registry.async_get_registry(self.hass)
+        devt_entry = ent_reg.async_get_or_create(
+            "device_tracker",
+            DOMAIN,
+            user_input[ATTR_DEVICE_ID],
+            suggested_object_id=user_input[ATTR_DEVICE_NAME],
+        )
+        await person.async_add_user_device_tracker(
+            self.hass, user_input[CONF_USER_ID], devt_entry.entity_id
+        )
+
         return self.async_create_entry(
             title=user_input[ATTR_DEVICE_NAME], data=user_input
         )
